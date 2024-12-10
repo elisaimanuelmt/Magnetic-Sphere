48ca0ceb982f6e33713d02e815111b054930bcf6
Elisa Imanuel Liminto <elisaimanuelmt07@gmail.com>
2024-12-10 23:51:26 +0700

    second revision

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,26 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
+
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 3000); // Ganti gambar setiap 5 detik
 }
 
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+  slideIndex += n;
+  showSlides(slideIndex);
 }
